ok-ryoko/turret

commit 6f6c72c3909dd598e0d43fcae76456159dcf7f46

improve user specification semantics
Try to reconcile the facts that 0 is the default value of the uint type
in Go as well as the value of the root UID in Linux without fixing an
arbitrary nonzero default value. Thus, make it possible to delegate the
choice of UID to the user-space utility responsible for creating the
user. In addition, disallow the omission of the `user.name` field in the
spec format in favor of a more explicit style.

BREAKING CHANGE: change `user.uid` to `user.id` in the spec format
BREAKING CHANGE: remove `user.create` from the spec format
BREAKING CHANGE: remove `builder.DefaultUser` and `builder.NewSpec`

pkg/builder/spec.go
@@ -35,7 +35,7 @@ type Spec struct {
 	Packages Packages
 
 	// Sole unprivileged user in the working container
-	User User
+	User *User
 
 	// Map of destination paths in the working container's file system to
 	// sources in the end user's home directory
@@ -63,8 +63,10 @@ func (s *Spec) Fill() {
 		}
 	}
 
-	if s.User.LoginShell == "" {
-		s.User.LoginShell = s.Distro.DefaultShell()
+	if s.User != nil {
+		if s.User.LoginShell == "" {
+			s.User.LoginShell = s.Distro.DefaultShell()
+		}
 	}
 }
 
@@ -98,7 +100,7 @@ func (s *Spec) Validate() error {
 		}
 	}
 
-	if s.User.Create {
+	if s.User != nil {
 		re1, err := regexp.Compile(rePOSIXPortableName)
 		if err != nil {
 			return fmt.Errorf("compiling regular expression: %w", err)
@@ -123,8 +125,8 @@ func (s *Spec) Validate() error {
 			}
 		}
 
-		if s.User.UID < 1000 || s.User.UID > 60000 {
-			return fmt.Errorf("UID %d outside allowed range [1000-60000]", s.User.UID)
+		if s.User.ID != 0 && (s.User.ID < 1000 || s.User.ID > 60000) {
+			return fmt.Errorf("UID %d outside allowed range [1000-60000]", s.User.ID)
 		}
 
 		if len(s.User.Groups) > 0 {
@@ -149,23 +151,6 @@ func (s *Spec) Validate() error {
 	}
 
 	return nil
-}
-
-// NewSpec generates a default but invalid spec
-func NewSpec() Spec {
-	return Spec{
-		Distro:      linux.DistroWrapper{Distro: 0},
-		Repository:  "",
-		Tag:         "",
-		From:        BaseImage{},
-		Packages:    Packages{},
-		User:        DefaultUser(),
-		Copy:        map[string][]string{},
-		Env:         map[string]string{},
-		Annotations: map[string]string{},
-		KeepHistory: false,
-		Security:    Security{},
-	}
 }
 
 // BaseImage holds the components of the base image reference
@@ -197,14 +182,16 @@ type Packages struct {
 
 // User holds information about a Linux user
 type User struct {
-	// Whether to create this user
-	Create bool
-
 	// Human-readable identifier
 	Name string
 
-	// Numeric identifier from 1000 to 60000, inclusive
-	UID uint `toml:"uid"`
+	// Linux user ID (UID)
+	//
+	// The default value of 0 tells the program to delegate the choice of UID
+	// to the user-space utility responsible for user creation
+	//
+	// If not 0, then it must be an integer between 1000 and 60000, inclusive
+	ID uint `toml:"id"`
 
 	// Whether to create a user group
 	UserGroup bool `toml:"user-group"`
@@ -217,19 +204,6 @@ type User struct {
 
 	// Login shell; must be a PATH-resolvable executable
 	LoginShell string `toml:"login-shell"`
-}
-
-// DefaultUser returns a minimally configured Linux user
-func DefaultUser() User {
-	return User{
-		Create:     false,
-		Name:       "user",
-		UID:        1000,
-		UserGroup:  false,
-		Groups:     []string{},
-		Comment:    "",
-		LoginShell: "",
-	}
 }
 
 // Security holds security-related options for the working container

cmd/turret/build.go
@@ -184,9 +184,9 @@ func newBuildCmd(logger *logrus.Logger) *cli.Command {
 				logger.Debugln("package cache cleaning step succeeded")
 			}
 
-			if spec.User.Create {
+			if spec.User != nil {
 				createUserOptions := builder.CreateUserOptions{
-					UID:        spec.User.UID,
+					ID:         spec.User.ID,
 					UserGroup:  spec.User.UserGroup,
 					Groups:     spec.User.Groups,
 					Comment:    spec.User.Comment,
@@ -225,7 +225,7 @@ func newBuildCmd(logger *logrus.Logger) *cli.Command {
 				LoginShell:  spec.User.LoginShell,
 				UserName:    spec.User.Name,
 			}
-			b.Configure(spec.User.Create, configureOptions)
+			b.Configure(spec.User != nil, configureOptions)
 			logger.Debugln("configured image")
 
 			logger.Debugln("committing image...")
@@ -299,7 +299,7 @@ func createSpec(p string, hash bool) (builder.Spec, string, error) {
 	d := toml.NewDecoder(r)
 	d.DisallowUnknownFields()
 
-	spec := builder.NewSpec()
+	spec := builder.Spec{}
 	if err = d.Decode(&spec); err != nil {
 		return builder.Spec{}, "", fmt.Errorf("decoding TOML: %w", err)
 	}

examples/alpine.toml
@@ -7,4 +7,4 @@ repository = "docker.io/library/alpine"
 tag = "3.17.3"
 
 [user]
-create = true
+name = "user"

examples/arch.toml
@@ -7,7 +7,7 @@ repository = "docker.io/library/archlinux"
 tag = "base-20230514.0.150299"
 
 [user]
-create = true
+name = "user"
 
 [security.special-files]
 remove-s = true

examples/debian.toml
@@ -7,7 +7,7 @@ repository = "docker.io/library/debian"
 tag = "11.7-slim"
 
 [user]
-create = true
+name = "user"
 
 [security.special-files]
 remove-s = true

examples/fedora.toml
@@ -7,7 +7,7 @@ repository = "registry.fedoraproject.org/fedora"
 tag = "38-x86_64"
 
 [user]
-create = true
+name = "user"
 
 [security.special-files]
 remove-s = true

examples/opensuse.toml
@@ -7,7 +7,7 @@ repository = "registry.opensuse.org/opensuse/leap"
 tag = "15.5"
 
 [user]
-create = true
+name = "user"
 
 [security.special-files]
 remove-s = true

examples/void.toml
@@ -7,7 +7,7 @@ repository = "ghcr.io/void-linux/void-linux"
 tag = "20230204RC01-full-x86_64"
 
 [user]
-create = true
+name = "user"
 
 [security.special-files]
 remove-s = true

commit 29576dd2daae520730ed8d98620b462a95abe425

build: mitigate nil map access
Initialize maps in the spec to an empty map when the corresponding
spec field has not been filled out

Fixes a regression introduced by commit 6f6c72c

pkg/builder/spec.go
@@ -69,6 +69,18 @@ func (s *Spec) Fill() {
 			s.User.LoginShell = s.Distro.DefaultShell()
 		}
 	}
+
+	if s.Annotations == nil {
+		s.Annotations = map[string]string{}
+	}
+
+	if s.Copy == nil {
+		s.Copy = map[string][]string{}
+	}
+
+	if s.Env == nil {
+		s.Env = map[string]string{}
+	}
 }
 
 // Validate asserts that the spec is suitable for ingestion by a builder.