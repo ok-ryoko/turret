93a08b5ea976175a40029a64138796116bbf6ded
OK Ryoko <ryoko@kyomu.jp.net>
2023-07-26 13:44:55 -0400

    don't export types from commands
    BREAKING CHANGE: de-export UTCFormatter and ISO8601 types

--- cmd/turret/logger.go
+++ cmd/turret/logger.go
@@ -11,24 +11,24 @@ import (
 
 func newLogger() *logrus.Logger {
 	logger := logrus.New()
-	logger.SetFormatter(UTCFormatter{
+	logger.SetFormatter(utcFormatter{
 		&logrus.TextFormatter{
 			DisableColors:          true,
 			DisableLevelTruncation: true,
 			FullTimestamp:          true,
-			TimestampFormat:        ISO8601,
+			TimestampFormat:        iso8601,
 		},
 	})
 	return logger
 }
 
-const ISO8601 = "2006-01-02T15:04:05Z"
+const iso8601 = "2006-01-02T15:04:05Z"
 
-type UTCFormatter struct {
+type utcFormatter struct {
 	logrus.Formatter
 }
 
-func (u UTCFormatter) Format(e *logrus.Entry) ([]byte, error) {
+func (u utcFormatter) Format(e *logrus.Entry) ([]byte, error) {
 	e.Time = e.Time.UTC()
 	result, err := u.Formatter.Format(e)
 	if err != nil {
