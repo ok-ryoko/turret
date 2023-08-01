ok-ryoko/turret

commit e1b9a7281d651fd92cc9ce713421d92ffce5a000

builder: extract user and group management data
Introduces the `TurretUserManagerInterface` type to represent a user
and group management interface over Buildah. Implementors of this
interface are expected to consume data generated by the command
factories in the new `usrgrp` package. Moreover, removes the last
remaining distro-specific implementation of `TurretBuilderInterface`,
retaining the interface in the event that it is needed again before
development enters the alpha stage.

cmd/turret/build.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/ok-ryoko/turret/pkg/builder"
+	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
 
 	"github.com/containers/storage"
 	"github.com/containers/storage/pkg/unshare"
@@ -133,12 +134,14 @@ func newBuildCmd(logger *logrus.Logger) *cli.Command {
 
 			distro := spec.Distro.Distro
 			packageManager := spec.Packages.Manager.Manager
+			userManager := spec.User.Manager.Manager
 			baseRef := spec.From.Reference()
 			commonOptions := builder.CommonOptions{LogCommands: v >= 4}
 			b, err := builder.New(
 				ctx,
 				distro,
 				packageManager,
+				userManager,
 				baseRef,
 				cCtx.Bool("pull"),
 				store,
@@ -182,14 +185,14 @@ func newBuildCmd(logger *logrus.Logger) *cli.Command {
 			}
 
 			if spec.User != nil {
-				createUserOptions := builder.CreateUserOptions{
+				createUserOptions := usrgrp.CreateUserOptions{
 					ID:         spec.User.ID,
 					UserGroup:  spec.User.UserGroup,
 					Groups:     spec.User.Groups,
 					Comment:    spec.User.Comment,
 					LoginShell: spec.User.LoginShell,
 				}
-				if err = b.CreateUser(spec.User.Name, spec.Distro.Distro, createUserOptions); err != nil {
+				if err = b.CreateUser(spec.User.Name, createUserOptions); err != nil {
 					return fmt.Errorf("creating unprivileged user: %w", err)
 				}
 				logger.Debugf("created unprivileged user '%s'", spec.User.Name)

pkg/builder/builder.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/ok-ryoko/turret/pkg/linux"
 	"github.com/ok-ryoko/turret/pkg/linux/pckg"
+	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
 
 	"github.com/containers/buildah"
 	is "github.com/containers/image/v5/storage"
@@ -51,7 +52,7 @@ type TurretBuilderInterface interface {
 	CopyFiles(destSourcesMap map[string][]string, options CopyFilesOptions) error
 
 	// CreateUser creates the sole unprivileged user of the working container.
-	CreateUser(name string, distro linux.Distro, options CreateUserOptions) error
+	CreateUser(name string, options usrgrp.CreateUserOptions) error
 
 	// InstallPackages installs one or more packages in the working container.
 	InstallPackages(packages []string) error
@@ -76,6 +77,10 @@ type TurretBuilder struct {
 
 	// Pointer to an object that manages packages in the working container
 	PackageManager TurretPackageManagerInterface
+
+	// Pointer to an object that manages users and groups in the working
+	// container
+	UserManager TurretUserManagerInterface
 
 	// Common options available to all build steps
 	CommonOptions CommonOptions
@@ -157,8 +162,12 @@ func (b *TurretBuilder) Configure(user bool, options ConfigureOptions) {
 	b.Builder.SetOS("linux")
 
 	if user {
-		b.Builder.SetEntrypoint([]string{"/bin/sh", "-c"})
-		b.Builder.SetCmd([]string{options.LoginShell})
+		ep := []string{"/bin/sh"}
+		if options.LoginShell != "" {
+			ep = append(ep, "-c")
+			b.Builder.SetCmd([]string{options.LoginShell})
+		}
+		b.Builder.SetEntrypoint(ep)
 		b.Builder.SetUser(options.UserName)
 		b.Builder.SetWorkDir(filepath.Join("/home", options.UserName))
 	}
@@ -225,111 +234,28 @@ type CopyFilesOptions struct {
 
 // CreateUser creates the sole unprivileged user of the working container,
 // asserting that `name` is a nonempty string.
-func (b *TurretBuilder) CreateUser(name string, distro linux.Distro, options CreateUserOptions) error {
+func (b *TurretBuilder) CreateUser(name string, options usrgrp.CreateUserOptions) error {
 	if name == "" {
 		return fmt.Errorf("blank user name")
 	}
 
-	useraddCmd := []string{"useradd", "--create-home"}
+	if options.ID != 0 && (options.ID < 1000 || options.ID > 60000) {
+		return fmt.Errorf("UID %d outside allowed range [1000-60000]", options.ID)
+	}
 
-	if options.LoginShell != distro.DefaultShell() {
-		shell, err := b.resolveExecutable(options.LoginShell, distro)
+	if options.LoginShell != "" {
+		shell, err := b.resolveExecutable(options.LoginShell)
 		if err != nil {
-			return fmt.Errorf("resolving login shell: %w", err)
+			return fmt.Errorf("resolving shell: %w", err)
 		}
 		options.LoginShell = shell
 	}
-	useraddCmd = append(useraddCmd, "--shell", options.LoginShell)
 
-	if options.ID != 0 {
-		if options.ID < 1000 || options.ID > 60000 {
-			return fmt.Errorf("UID %d outside allowed range [1000-60000]", options.ID)
-		}
-		useraddCmd = append(
-			useraddCmd,
-			"--uid",
-			fmt.Sprintf("%d", options.ID),
-		)
-	}
-
-	userGroupFlag := "--no-user-group"
-	if options.UserGroup {
-		userGroupFlag = "--user-group"
-	}
-	useraddCmd = append(useraddCmd, userGroupFlag)
-
-	if options.Comment != "" {
-		useraddCmd = append(useraddCmd, "--comment", options.Comment)
-	}
-
-	if len(options.Groups) > 0 {
-		useraddCmd = append(
-			useraddCmd,
-			"--groups",
-			strings.Join(options.Groups, ","),
-		)
-	}
-
-	useraddCmd = append(useraddCmd, name)
-
-	uao := b.defaultRunOptions()
-
-	// CAP_DAC_READ_SEARCH and CAP_FSETID are elements of the useradd effective
-	// capability set but are not needed for the operation to succeed.
-	//
-	uao.AddCapabilities = []string{
-		"CAP_CHOWN",
-		//
-		// - Change owner of files copied from /etc/skel to /home/user
-		// - Change owner of /var/spool/mail/user
-
-		"CAP_DAC_OVERRIDE",
-		//
-		// - Open /etc/shadow and /etc/gshadow
-		// - Open files copied from /etc/skel to /home/user
-
-		"CAP_FOWNER",
-		//
-		// - Change owner and mode of temporary files when updating the passwd,
-		// shadow, gshadow, group, subuid and subgid files in /etc
-		// - Change owner and mode of /home/user and /var/spool/mail/user
-		// - Change owner of, set extended attributes on and update timestamps
-		// of files copied from /etc/skel to /home/user
-	}
-
-	// If the sss_cache command is available, then useradd will fork into
-	// sss_cache to invalidate the System Security Services Daemon cache,
-	// an operation that requires additional capabilities.
-	//
-	_, err := b.resolveExecutable("sss_cache", distro)
-	if err != nil {
-		b.Logger.Debugln("sss_cache not found; skipping cache invalidation")
-	} else {
-		uao.AddCapabilities = append(
-			uao.AddCapabilities,
-			"CAP_SETGID",
-			//
-			// Set the effective GID to 0 (root)
-
-			"CAP_SETUID",
-			//
-			// Set the effective UID to 0 (root)
-		)
-	}
-
-	if err := b.run(useraddCmd, uao); err != nil {
+	if err := b.UserManager.CreateUser(b, name, options); err != nil {
 		return fmt.Errorf("%w", err)
 	}
 
 	return nil
-}
-
-type CreateUserOptions struct {
-	ID         uint
-	UserGroup  bool
-	Groups     []string
-	Comment    string
-	LoginShell string
 }
 
 func (b *TurretBuilder) defaultRunOptions() buildah.RunOptions {
@@ -365,24 +291,15 @@ func (b *TurretBuilder) Remove() error {
 	if err != nil {
 		return fmt.Errorf("deleting container: %w", err)
 	}
-
-	b.Builder = nil
-	b.Logger = nil
-	b.CommonOptions = CommonOptions{}
-
+	*b = TurretBuilder{}
 	return nil
 }
 
 // resolveExecutable returns the absolute path of an executable in the working
 // container if it can be found and an error otherwise, assuming `command` can
 // be resolved.
-func (b *TurretBuilder) resolveExecutable(executable string, distro linux.Distro) (string, error) {
-	shell := distro.DefaultShell()
-	cmd := []string{shell}
-	if filepath.Base(shell) == "bash" {
-		cmd = append(cmd, "--restricted")
-	}
-	cmd = append(cmd, "-c", "command", "-v", executable)
+func (b *TurretBuilder) resolveExecutable(executable string) (string, error) {
+	cmd := []string{"/bin/sh", "-c", "command", "-v", executable}
 
 	var buf bytes.Buffer
 	ro := b.defaultRunOptions()
@@ -521,11 +438,12 @@ func (b *TurretBuilder) UpgradePackages() error {
 }
 
 // New creates a new TurretBuilder for a given combination of a Linux-based
-// distro and package manager.
+// distro, package manager, and user/group management utility.
 func New(
 	ctx context.Context,
 	distro linux.Distro,
 	packageManager pckg.Manager,
+	userManager usrgrp.Manager,
 	imageRef string,
 	pull bool,
 	store storage.Store,
@@ -556,39 +474,20 @@ func New(
 		return nil, fmt.Errorf("creating package manager: %w", err)
 	}
 
-	var tb TurretBuilderInterface
-	switch distro {
-	case linux.Alpine:
-		tb = &AlpineTurretBuilder{
-			TurretBuilder{
-				Builder:        b,
-				PackageManager: pm,
-				Logger:         logger,
-				CommonOptions:  options,
-			},
-		}
-	case linux.Debian:
-		options.Env = append(options.Env, "DEBIAN_FRONTEND=noninteractive")
-		tb = &TurretBuilder{
-			Builder:        b,
-			PackageManager: pm,
-			Logger:         logger,
-			CommonOptions:  options,
-		}
-	case
-		linux.Arch,
-		linux.Chimera,
-		linux.Fedora,
-		linux.OpenSUSE,
-		linux.Void:
-		tb = &TurretBuilder{
-			Builder:        b,
-			PackageManager: pm,
-			Logger:         logger,
-			CommonOptions:  options,
-		}
-	default:
-		return nil, fmt.Errorf("unrecognized distro")
+	um, err := NewUserManager(userManager)
+	if err != nil {
+		return nil, fmt.Errorf("creating user and group manager: %w", err)
 	}
-	return tb, nil
+
+	if distro == linux.Debian {
+		options.Env = append(options.Env, "DEBIAN_FRONTEND=noninteractive")
+	}
+
+	return &TurretBuilder{
+		Builder:        b,
+		PackageManager: pm,
+		UserManager:    um,
+		Logger:         logger,
+		CommonOptions:  options,
+	}, nil
 }

spec.toml
@@ -104,6 +104,12 @@
 #
 #login-shell = ""
 
+# The user and group management utility in the base image;
+# one of "busybox" and "shadow";
+# case-insensitive
+#
+#manager = ""
+
 [security.special-files]
 
 # Whether to automatically unset the special bit on any files in the working

pkg/builder/spec.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/ok-ryoko/turret/pkg/linux"
 	"github.com/ok-ryoko/turret/pkg/linux/pckg"
+	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
 )
 
 const (
@@ -64,9 +65,9 @@ func (s *Spec) Fill() {
 		}
 	}
 
-	if s.User != nil {
-		if s.User.LoginShell == "" {
-			s.User.LoginShell = s.Distro.DefaultShell()
+	if s.User.Manager.Manager == 0 {
+		s.User.Manager = usrgrp.ManagerWrapper{
+			Manager: s.Distro.DefaultUserManager(),
 		}
 	}
 
@@ -204,6 +205,9 @@ type User struct {
 
 	// Login shell; must be a PATH-resolvable executable
 	LoginShell string `toml:"login-shell"`
+
+	// Choice of user-space utility for managing users and groups
+	Manager usrgrp.ManagerWrapper
 }
 
 // Security holds security-related options for the working container.

pkg/builder/builder_alpine.go
@@ -1,83 +1,0 @@
-// Copyright 2023 OK Ryoko
-// SPDX-License-Identifier: Apache-2.0
-
-package builder
-
-import (
-	"fmt"
-
-	"github.com/ok-ryoko/turret/pkg/linux"
-)
-
-type AlpineTurretBuilder struct {
-	TurretBuilder
-}
-
-func (b *AlpineTurretBuilder) CreateUser(name string, distro linux.Distro, options CreateUserOptions) error {
-	if name == "" {
-		return fmt.Errorf("blank user name")
-	}
-
-	adduserCmd := []string{"adduser", "-D"}
-
-	if !options.UserGroup {
-		adduserCmd = append(adduserCmd, "-G", "users")
-	}
-
-	if options.LoginShell != distro.DefaultShell() {
-		shell, err := b.resolveExecutable(options.LoginShell, distro)
-		if err != nil {
-			return fmt.Errorf("resolving login shell: %w", err)
-		}
-		options.LoginShell = shell
-	}
-	adduserCmd = append(adduserCmd, "-s", options.LoginShell)
-
-	if options.ID != 0 {
-		if options.ID < 1000 || options.ID > 60000 {
-			return fmt.Errorf("UID %d outside allowed range [1000-60000]", options.ID)
-		}
-		adduserCmd = append(
-			adduserCmd,
-			"-u",
-			fmt.Sprintf("%d", options.ID),
-		)
-	}
-
-	if options.Comment != "" {
-		adduserCmd = append(adduserCmd, "-g", options.Comment)
-	}
-
-	adduserCmd = append(adduserCmd, name)
-
-	// CAP_DAC_OVERRIDE and CAP_FSETID are elements of the useradd effective
-	// capability set but are not needed for the operation to succeed
-	//
-	auo := b.defaultRunOptions()
-	auo.AddCapabilities = []string{
-		"CAP_CHOWN",
-		//
-		// Change owner of /home/user
-
-		"CAP_FOWNER",
-		//
-		// Change mode and owner of /home/user as well as temporary files when
-		// editing /etc/passwd, /etc/shadow and /etc/group
-	}
-
-	if err := b.run(adduserCmd, auo); err != nil {
-		return fmt.Errorf("%w", err)
-	}
-
-	if len(options.Groups) > 0 {
-		ago := b.defaultRunOptions()
-		for _, g := range options.Groups {
-			addgroupCmd := []string{"addgroup", name, g}
-			if err := b.run(addgroupCmd, ago); err != nil {
-				return fmt.Errorf("%w", err)
-			}
-		}
-	}
-
-	return nil
-}

pkg/builder/usermanager.go
@@ -1,0 +1,43 @@
+// Copyright 2023 OK Ryoko
+// SPDX-License-Identifier: Apache-2.0
+
+package builder
+
+import (
+	"fmt"
+
+	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
+)
+
+// TurretUserManagerInterface is the interface implemented by a
+// TurretUserManager for a particular user and group management utility.
+type TurretUserManagerInterface interface {
+	// CreateUser creates the sole unprivileged user of the working container.
+	CreateUser(b *TurretBuilder, name string, options usrgrp.CreateUserOptions) error
+}
+
+// TurretUserManager provides a high-level front end for Buildah for managing
+// users and groups in a Linux builder container.
+type TurretUserManager struct {
+	usrgrp.CommandFactory
+}
+
+// NewUserManager creates a new TurretUserManager for a particular user and
+// group management utility.
+func NewUserManager(um usrgrp.Manager) (TurretUserManagerInterface, error) {
+	cf, err := usrgrp.NewCommandFactory(um)
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	var tum TurretUserManagerInterface
+	switch um {
+	case usrgrp.BusyBox:
+		tum = &BusyBoxTurretUserManager{TurretUserManager{cf}}
+	case usrgrp.Shadow:
+		tum = &ShadowTurretUserManager{TurretUserManager{cf}}
+	default:
+		return nil, fmt.Errorf("unrecognized package manager")
+	}
+	return tum, nil
+}

pkg/builder/usermanager_busybox.go
@@ -1,0 +1,56 @@
+// Copyright 2023 OK Ryoko
+// SPDX-License-Identifier: Apache-2.0
+
+package builder
+
+import (
+	"fmt"
+
+	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
+)
+
+type BusyBoxTurretUserManager struct {
+	TurretUserManager
+}
+
+// CreateUser creates the sole unprivileged user of the working container.
+func (um *BusyBoxTurretUserManager) CreateUser(b *TurretBuilder, name string, options usrgrp.CreateUserOptions) error {
+	cmd, capabilities := um.NewCreateUserCmd(name, options)
+	ro := b.defaultRunOptions()
+	ro.AddCapabilities = capabilities
+	if err := b.run(cmd, ro); err != nil {
+		return fmt.Errorf(
+			"creating user using %s: %w",
+			um.UserManager().String(),
+			err,
+		)
+	}
+
+	if options.UserGroup {
+		cmd, _ = um.NewAddUserToGroupCmd(name, name)
+		ro = b.defaultRunOptions()
+		if err := b.run(cmd, ro); err != nil {
+			return fmt.Errorf(
+				"adding user to group using %s: %w",
+				um.UserManager().String(),
+				err,
+			)
+		}
+	}
+
+	if len(options.Groups) > 0 {
+		for _, g := range options.Groups {
+			cmd, _ = um.NewAddUserToGroupCmd(name, g)
+			ro = b.defaultRunOptions()
+			if err := b.run(cmd, ro); err != nil {
+				return fmt.Errorf(
+					"adding user to group using %s: %w",
+					um.UserManager().String(),
+					err,
+				)
+			}
+		}
+	}
+
+	return nil
+}

pkg/builder/usermanager_shadow.go
@@ -1,0 +1,51 @@
+// Copyright 2023 OK Ryoko
+// SPDX-License-Identifier: Apache-2.0
+
+package builder
+
+import (
+	"fmt"
+
+	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
+)
+
+type ShadowTurretUserManager struct {
+	TurretUserManager
+}
+
+// CreateUser creates the sole unprivileged user of the working container.
+func (um *ShadowTurretUserManager) CreateUser(b *TurretBuilder, name string, options usrgrp.CreateUserOptions) error {
+	cmd, capabilities := um.NewCreateUserCmd(name, options)
+	ro := b.defaultRunOptions()
+	ro.AddCapabilities = capabilities
+
+	// If the sss_cache command is available, then useradd will fork into
+	// sss_cache to invalidate the System Security Services Daemon cache,
+	// an operation that requires additional capabilities.
+	//
+	_, err := b.resolveExecutable("sss_cache")
+	if err != nil {
+		b.Logger.Debugln("sss_cache not found; skipping cache invalidation")
+	} else {
+		ro.AddCapabilities = append(
+			ro.AddCapabilities,
+			"CAP_SETGID",
+			//
+			// Set the effective GID to 0 (root)
+
+			"CAP_SETUID",
+			//
+			// Set the effective UID to 0 (root)
+		)
+	}
+
+	if err := b.run(cmd, ro); err != nil {
+		return fmt.Errorf(
+			"creating user using %s: %w",
+			um.UserManager().String(),
+			err,
+		)
+	}
+
+	return nil
+}

commit 9fd1817086ce3bfc15ef01d4ae8d9386d6b19cd2

restructure spec format
Moves implementation strategies in the spec into a new `backends` table
to enforce separation of data and logic

Fixes regression introduced by commit e1b9a72

BREAKING CHANGE: move `packages.manager` to `backends.package`
BREAKING CHANGE: move `user.manager` to `backends.user`
BREAKING CHANGE: update `builder.Builder.Configure` call signature
BREAKING CHANGE: remove `UserName` and `LoginShell` fields from `builder.ConfigureOptions`

cmd/turret/build.go
@@ -135,8 +135,8 @@ func newBuildCmd(logger *logrus.Logger) *cli.Command {
 			}
 
 			distro := spec.Distro.Distro
-			packageManager := spec.Packages.Manager.Manager
-			userManager := spec.User.Manager.Manager
+			packageManager := spec.Backends.Package.Manager
+			userManager := spec.Backends.User.Manager
 			baseRef := spec.From.Reference()
 			commonOptions := container.CommonOptions{LogCommands: v >= 4}
 			b, err := builder.New(
@@ -224,10 +224,9 @@ func newBuildCmd(logger *logrus.Logger) *cli.Command {
 			configureOptions := builder.ConfigureOptions{
 				Annotations: spec.Annotations,
 				Env:         spec.Env,
-				LoginShell:  spec.User.LoginShell,
-				UserName:    spec.User.Name,
+				User:        spec.User,
 			}
-			b.Configure(spec.User != nil, configureOptions)
+			b.Configure(configureOptions)
 			logger.Debugln("configured image")
 
 			logger.Debugln("committing image...")

pkg/builder/builder.go
@@ -16,6 +16,7 @@ import (
 	"github.com/ok-ryoko/turret/pkg/linux"
 	"github.com/ok-ryoko/turret/pkg/linux/pckg"
 	"github.com/ok-ryoko/turret/pkg/linux/usrgrp"
+	"github.com/ok-ryoko/turret/pkg/spec"
 
 	"github.com/containers/buildah"
 	is "github.com/containers/image/v5/storage"
@@ -96,18 +97,15 @@ type CommitOptions struct {
 }
 
 // Configure sets metadata and runtime parameters for the working container.
-func (b *Builder) Configure(user bool, options ConfigureOptions) {
+func (b *Builder) Configure(options ConfigureOptions) {
 	b.Builder.SetOS("linux")
 
-	if user {
-		ep := []string{"/bin/sh"}
-		if options.LoginShell != "" {
-			ep = append(ep, "-c")
-			b.Builder.SetCmd([]string{options.LoginShell})
+	if options.User != nil {
+		if options.User.LoginShell != "" {
+			b.Builder.SetCmd([]string{options.User.LoginShell})
 		}
-		b.Builder.SetEntrypoint(ep)
-		b.Builder.SetUser(options.UserName)
-		b.Builder.SetWorkDir(filepath.Join("/home", options.UserName))
+		b.Builder.SetUser(options.User.Name)
+		b.Builder.SetWorkDir(filepath.Join("/home", options.User.Name))
 	}
 
 	for k, v := range options.Env {
@@ -122,8 +120,7 @@ func (b *Builder) Configure(user bool, options ConfigureOptions) {
 type ConfigureOptions struct {
 	Annotations map[string]string
 	Env         map[string]string
-	LoginShell  string
-	UserName    string
+	User        *spec.User
 }
 
 // CopyFiles copies one or more files from the end user's home directory to the

spec.toml
@@ -51,12 +51,6 @@
 
 [packages]
 
-# manager is the package manager in the base image;
-# one of "apk", "apt", "dnf", "pacman", "xbps" and "zypper";
-# case-insensitive
-#
-#manager = ""
-
 # Whether to upgrade pre-installed packages
 #
 #upgrade = false
@@ -104,12 +98,6 @@
 #
 #login-shell = ""
 
-# The user and group management utility in the base image;
-# one of "busybox" and "shadow";
-# case-insensitive
-#
-#manager = ""
-
 [security.special-files]
 
 # Whether to automatically unset the special bit on any files in the working
@@ -123,3 +111,17 @@
 # file need not possess a special bit to be listed)
 #
 #excludes = []
+
+[backends]
+
+# The package manager in the base image;
+# one of "apk", "apt", "dnf", "pacman", "xbps" and "zypper";
+# case-insensitive
+#
+#package = ""
+
+# The user and group management utility in the base image;
+# one of "busybox" and "shadow";
+# case-insensitive
+#
+#user = ""

pkg/spec/spec.go
@@ -54,19 +54,22 @@ type Spec struct {
 
 	// Security options for the working container
 	Security Security
+
+	// Choices of implementations of operations in the working container
+	Backends Backends
 }
 
 // Fill populates empty optional fields in the spec using information encoded
 // by required fields.
 func (s *Spec) Fill() {
-	if s.Packages.Manager.Manager == 0 {
-		s.Packages.Manager = pckg.ManagerWrapper{
+	if s.Backends.Package.Manager == 0 {
+		s.Backends.Package = pckg.ManagerWrapper{
 			Manager: s.Distro.DefaultPackageManager(),
 		}
 	}
 
-	if s.User.Manager.Manager == 0 {
-		s.User.Manager = usrgrp.ManagerWrapper{
+	if s.Backends.User.Manager == 0 {
+		s.Backends.User = usrgrp.ManagerWrapper{
 			Manager: s.Distro.DefaultUserManager(),
 		}
 	}
@@ -90,8 +93,12 @@ func (s *Spec) Validate() error {
 		return fmt.Errorf("missing distro")
 	}
 
-	if s.Packages.Manager.Manager == 0 {
+	if s.Backends.Package.Manager == 0 {
 		return fmt.Errorf("missing package manager")
+	}
+
+	if s.Backends.User.Manager == 0 {
+		return fmt.Errorf("missing user/group management utility")
 	}
 
 	if s.Repository == "" {
@@ -103,7 +110,7 @@ func (s *Spec) Validate() error {
 	}
 
 	if len(s.Packages.Install) > 0 {
-		re := regexp.MustCompile(s.Packages.Manager.RePackageName())
+		re := regexp.MustCompile(s.Backends.Package.RePackageName())
 		for _, p := range s.Packages.Install {
 			if !re.MatchString(p) {
 				return fmt.Errorf("invalid package name '%s'", p)
@@ -168,9 +175,6 @@ func (i BaseImage) Reference() string {
 
 // Packages contains instructions for the distro's canonical package manager.
 type Packages struct {
-	// Identity of the package manager in the working container
-	Manager pckg.ManagerWrapper
-
 	// Whether to upgrade pre-installed packages
 	Upgrade bool
 
@@ -205,9 +209,6 @@ type User struct {
 
 	// Login shell; must be a PATH-resolvable executable
 	LoginShell string `toml:"login-shell"`
-
-	// Choice of user-space utility for managing users and groups
-	Manager usrgrp.ManagerWrapper
 }
 
 // Security holds security-related options for the working container.
@@ -223,4 +224,15 @@ type SpecialFiles struct {
 
 	// Whether to preserve the SUID/SGID bit on one or more files
 	Excludes []string
+}
+
+// Backends holds the choices of implementations of operations in the working
+// container
+type Backends struct {
+	// Identity of the package manager in the working container
+	Package pckg.ManagerWrapper
+
+	// Identity of user-space utility for managing users and groups in the
+	// working container
+	User usrgrp.ManagerWrapper `toml:"user-group"`
 }